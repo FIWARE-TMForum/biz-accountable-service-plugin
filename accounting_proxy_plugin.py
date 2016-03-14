0548658b3c8ddb4c4d8c0d53741f4af64444db3c
victor <vramperez@conwet.com>
2016-03-14 16:25:53 +0100

    Fix errors and update paths.

--- accounting_proxy_plugin.py
+++ accounting_proxy_plugin.py
@@ -16,7 +16,7 @@ class AccountingProxyPlugin(Plugin):
 
     def on_post_product_spec_validation(self, provider, asset):
         # Check the url
-        url = 'http://' + urlparse(asset.get_url()).netloc + '/api/resources'
+        url = urlparse(asset.get_url()).scheme + '://' + urlparse(asset.get_url()).netloc + '/accounting_proxy/urls'
         payload = {'url': asset.get_url()}
 
         resp = requests.post(url, json=payload)
@@ -25,43 +25,58 @@ class AccountingProxyPlugin(Plugin):
             raise PluginError('Invalid asset url')
 
 
-    def on_pre_product_offering_validation(self, asset, product_offering):
+    def on_post_product_offering_validation(self, asset, product_offering):
         # Check supported accounting units
-        url = 'http://' + urlparse(asset.get_url()).netloc + '/api/units'
+        url = urlparse(asset.get_url()).scheme + '://' + urlparse(asset.get_url()).netloc + '/accounting_proxy/units'
 
         resp = requests.get(url)
 
         if resp.status_code != 200:
-            raise PlugginError('Error checking the supported accounting units')
+            raise PluginError('Error checking the supported accounting units')
         else:
-            units = json.loads(resp.text)['units']
+            units = resp.json()['units']
+            found = False
 
             for price_model in product_offering['productOfferingPrice']:
 
                 if price_model['priceType'] == 'usage' and price_model['unitOfMeasure'] not in units:
                     raise PluginError('Unsupported accounting unit ' + price_model['unit'] + '. Supported units are: ' + units)
+                if price_model['priceType'] == 'usage' and price_model['unitOfMeasure'] in units:
+                    found = True
 
+            if not fount:
+                raise PluginError('No "usage" price type in the product_offering')
 
     def on_product_acquisition(self, asset, contract, order):
         # Send new buy notification to the accounting proxy
-        url = 'http://' + urlparse(asset.get_url()).netloc + '/api/users'
+        url = urlparse(asset.get_url()).scheme + '://' + urlparse(asset.get_url()).netloc + '/accounting_proxy/buys'
 
-        for price_model in contract.pricing_model:
-            if 'unit' in price_model:
-                unit = price_model['unit']
+        if 'pay per use' in contract.pricing_model:
 
-        payload = { 
-            'productId': asset.product_id,
-            'orderId': order.order_id,
-            'customer': order.customer,
-            'productSpecification': {
-                'url': order.offering.href,
-                'unit': unit,
-                'recordType': self.record_type(unit)
-            }
-        }
+            count = 0
+            for price_model in contract.pricing_model['pay per use']:
+                if 'unit' in price_model:
+                    unit = price_model['unit']
+                    count += 1
 
-        resp = requests.post(url, json=payload)
+            if count != 1:
+                raise PluginError('Wrong number of pricing components. Only supported one pricing component')
+            else:
 
-        if resp.status_code != 201:
-            raise PluginError('Error notifying the product acquisition to the accounting proxy')
+                payload = { 
+                    'productId': asset.product_id,
+                    'orderId': order.order_id,
+                    'customer': order.customer,
+                    'productSpecification': {
+                        'url': order.asset.get_url(),
+                        'unit': unit,
+                        'recordType': self.record_type(unit)
+                    }
+                }
+
+                resp = requests.post(url, json=payload)
+
+                if resp.status_code != 201:
+                    raise PluginError('Error notifying the product acquisition to the accounting proxy')
+        else 
+            raise PluginError('Contract must have a "pay per use" in the pricing_model')
