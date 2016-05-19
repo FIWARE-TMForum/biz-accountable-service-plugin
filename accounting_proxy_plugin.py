36066b2e7647c0d8a16fc3b4266b4ef280c3f483
fdelavega <fdelavega@conwet.com>
2016-05-19 13:35:28 +0200

    Remove the asset in post_product_validation handler in case of error to avoid an inconsistent state in the database

--- accounting_proxy_plugin.py
+++ accounting_proxy_plugin.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from wstore.asset_manager.resource_plugins.plugin import Plugin
 from wstore.asset_manager.resource_plugins.plugin_error import PluginError
 
+
 class AccountingProxyPlugin(Plugin):
 
     def record_type(self, unit):
@@ -16,37 +17,41 @@ class AccountingProxyPlugin(Plugin):
 
     def on_post_product_spec_validation(self, provider, asset):
         # Get the apiKey for accounting notifications
-        acc_proxy_url = urlparse(asset.get_url()).scheme + '://' + urlparse(asset.get_url()).netloc
-        authorize_url = settings.AUTHORIZE_SERVICE
+        try:
+            acc_proxy_url = urlparse(asset.get_url()).scheme + '://' + urlparse(asset.get_url()).netloc
+            authorize_url = settings.AUTHORIZE_SERVICE
 
-        resp = requests.post(authorize_url, json={'url': acc_proxy_url})
+            resp = requests.post(authorize_url, json={'url': acc_proxy_url})
 
-        if resp.status_code != 201:
-            raise PluginError('Error getting the api_key')
-
-        else:
-            # Check the url and send the apiKey
-            api_key = resp.json()['apiKey']
-
-            url = acc_proxy_url + '/accounting_proxy/urls'
-            headers = {'X-API-KEY': api_key}
-            payload = {'url': asset.get_url()}
-
-            resp = requests.post(url, headers=headers, json=payload)
-
-            if resp.status_code != 200:
-                raise PluginError('Invalid asset url')
+            if resp.status_code != 201:
+                raise PluginError('Error getting the api_key')
 
             else:
-                # Commit the apiKey received
-                authorize_url = settings.AUTHORIZE_SERVICE + '/' + api_key + '/commit' 
+                # Check the url and send the apiKey
+                api_key = resp.json()['apiKey']
 
-                resp = requests.post(authorize_url)
+                url = acc_proxy_url + '/accounting_proxy/urls'
+                headers = {'X-API-KEY': api_key}
+                payload = {'url': asset.get_url()}
 
-                if resp.status_code != 200: 
-                    raise PluginError('Error committing the api_key')
+                resp = requests.post(url, headers=headers, json=payload)
 
+                if resp.status_code != 200:
+                    raise PluginError('Invalid asset url')
 
+                else:
+                    # Commit the apiKey received
+                    authorize_url = settings.AUTHORIZE_SERVICE + '/' + api_key + '/commit'
+
+                    resp = requests.post(authorize_url)
+
+                    if resp.status_code != 200:
+                        raise PluginError('Error committing the api_key')
+
+        except Exception as e:
+            # Remove the asset to avoid an inconsistent state in  the database
+            asset.delete()
+            raise e
 
     def on_post_product_offering_validation(self, asset, product_offering):
         # Check supported accounting units
